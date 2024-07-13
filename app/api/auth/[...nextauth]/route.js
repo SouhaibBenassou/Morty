340f439432db06de211a190d3b8ca212409879df
Souby <Benassou.sohaib@gmail.com>
2024-07-13 13:35:40 +0100

    testsd

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { compare } from "bcrypt";
+
 import { sql } from "@vercel/postgres";
 
 const handler = NextAuth({
